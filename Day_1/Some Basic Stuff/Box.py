4e55776fb744bb46b83a1283de1c7aba6a629c2e
ARY2260 <arb.aryan@gmail.com>
2021-11-21 21:38:36 +0530

    notes and doubts day1

--- Day_1/Some Basic Stuff/Box.py
+++ Day_1/Some Basic Stuff/Box.py
@@ -1,16 +1,52 @@
+""" 
+    basic tasks: (to drop a cube from certain height)
+
+        - import dependencies
+        - connect to the client
+        - set inventory path (optional)
+        - set gravity
+        - load plane (.URDF)
+        - set object pos data
+        - set object orient data 
+        - load object (.URDF)
+        - start a simulation and set sleep time in a time loop
+        - get pos and orient values of the object
+        - execute other statements 
+        - disconnect from the client
+"""
 import pybullet as p
 import time
-import pybullet_data
-physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-p.setGravity(0,0,-10)
+import pybullet_data  # takes in built data from pybullet
+
+
+physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
+# optionally (to search for an existing file in pybullet library)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, 9.8)  # gravity setup
+
+
 planeId = p.loadURDF("plane.urdf")
-cubeStartPos = [0,0,1]
-cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("box.urdf",cubeStartPos, cubeStartOrientation)
-for i in range (10000):
-    p.stepSimulation()
-    time.sleep(1./240.)
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-print(cubePos,cubeOrn)
-p.disconnect()
+
+cubeStartPos = [0, 0, 1]  # spawn point
+cubeStartOrientation = p.getQuaternionFromEuler(
+    [0, 1.56, 0.57])  # rpy value at spawn
+
+# spawning fn : loadURDF(<file>,position,orientation)
+boxId = p.loadURDF("Box.urdf", cubeStartPos, cubeStartOrientation)
+# object id is stored in the variable that calls loadURDF method
+
+for i in range(100):  # defines the length of the simulation
+
+    p.stepSimulation()  # doubt: what is the length of one step?
+
+    time.sleep(1./240.)  # get simulation after every 1/240th of a sec
+# its a pause during a code execution to let other libs or executions run
+cubePos, cubeOrn = p.getBasePositionAndOrientation(
+    boxId)  # get fn for pos and orient values
+
+print(cubePos, " orientation = ", cubeOrn)
+
+p.disconnect()  # disconnect from client
+
+# doubt: even without p.diconnect(), the client window automatically closes
+# doubt : what is the 4th value in cube orientation values?
--- Day_1/Some Basic Stuff/Moving r2d2.py
+++ Day_1/Some Basic Stuff/Moving r2d2.py
@@ -1,22 +1,46 @@
+""" 
+    basic tasks: (move 2 r2d2 robot objs)
+
+        - import dependencies
+        - connect to the client
+        - set inventory path (optional)
+        - load plane (.URDF)
+        - set object1 pos data
+        - set object1 orient data 
+        - load object1 (.URDF)
+        - set object2 pos data
+        - set object2 orient data 
+        - load object2 (.URDF)
+        - set time loop
+        - manipulate gravity till a certain value of x is achieved
+        - set simulation and sleep time
+        - else case : remove old bodies and insert new ones
+        - disconnect from the client
+"""
 import pybullet as p
 import time
 import pybullet_data
+
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+
 planeId = p.loadURDF("plane.urdf")
+
 roboStartPos = [2,2,1]
 roboStartOrientation = p.getQuaternionFromEuler([0,0,0])
 roboId = p.loadURDF("r2d2.urdf",roboStartPos, roboStartOrientation)
+
 robo2StartPos = [0,0,1]
 robo2StartOrientation = p.getQuaternionFromEuler([0,0,0])
 robo2Id = p.loadURDF("r2d2.urdf",robo2StartPos, robo2StartOrientation)
+
 x=0.0
 for i in range (10000):
     if x<6.92:
      x=x+0.01
     else:
      x=0
-     p.removeBody(roboId) 
+     p.removeBody(roboId) # removeBody(<obj_id>) method to remove objs
      p.removeBody(robo2Id) 
      roboId = p.loadURDF("r2d2.urdf",roboStartPos, roboStartOrientation)
      robo2Id = p.loadURDF("r2d2.urdf",robo2StartPos, robo2StartOrientation)
